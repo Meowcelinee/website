ce9d597c5ecc3e6fb4ee30930a8da475f67e592b
Natalie Raine <nyatalieeee@proton.me>
2024-08-04 21:18:28 -0500

    Initial commit (again)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "natalie's site :3",
+    description: "made with next",
+};
---
+++ src/pages/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <p>hi :3</p>
+        </main>
+    );
+}
