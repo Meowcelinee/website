f58cce4cceba772f2a8eef3bc645397d03c844fe
Natalie Raine <nyatalieeee@proton.me>
2024-08-08 15:00:26 -0500

    rename to index

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { metadata } from "./_app";
+
+export default function Home() {
+    return (
+        <>
+            <div className="bg-base">
+                <p>hi :3</p>
+            </div>
+        </>
+    );
+}
--- src/pages/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <main>
-            <p>hi :3</p>
-        </main>
-    );
-}
