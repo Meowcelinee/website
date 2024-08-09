3dedf616d702082cb46505f0e6c516e3913e68fd
Natalie Raine <nyatalieeee@proton.me>
2024-08-09 16:41:44 -0500

    improve footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,11 @@
 export default function Footer() {
     return (
         <>
-            <div className="bg-mantle mt-3 md:py-4 md:px-3 py-3 px-2">
-                <p>site by natalie raine :3</p>
+            <div className="bg-mantle mt-3 mx-5 md:py-4 md:px-3 py-3 px-2">
+                <p className="mx-1">site by natalie raine :3</p>
                 <hr className="my-2 border-overlay0" />
-                <p>gock</p>
+                <p className="text-subtext0 text-sm mx-1">made with nextjs</p>
+                <p className="text-subtext0 text-sm mx-1">hosted by vercel</p>
             </div>
         </>
     );
