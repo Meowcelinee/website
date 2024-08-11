c95dd79a3ce0ddbaf18b4bea565ec8596c6f1b11
Natalie Raine <nyatalieeee@proton.me>
2024-08-11 10:49:35 -0500

    fix keys

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -5,29 +5,27 @@ import projects from '../../data/projectData';
 export default function Projects() {
     const projectList = projects.map((project) => {
         return (
-            <>
-                <div className="block bg-gradient-to-br from-mantle via-mantle to-crust rounded-lg drop-shadow-md py-3 px-3 mx-auto w-full">
-                    <div className="my-2">
-                        <Link
-                            href={project.link}
-                            className="text-xl text-pink font-semibold transition duration-300 hover:text-text"
-                            key={project.id}
-                        >
-                            {project.name}
-                        </Link>
-                    </div>
-                    <div>
-                        <ul className="text-sm list-disc mx-4">
-                            <li className="marker:text-pink" key={project.id}>
-                                {project.language}
-                            </li>
-                            <li className="marker:text-pink" key={project.id}>
-                                Date started: {project.date}
-                            </li>
-                        </ul>
-                    </div>
+            <div
+                className="block bg-gradient-to-br from-mantle via-mantle to-crust rounded-lg drop-shadow-md py-3 px-3 mx-auto w-full"
+                key={project.id}
+            >
+                <div className="my-2">
+                    <Link
+                        href={project.link}
+                        className="text-xl text-pink font-semibold transition duration-300 hover:text-text"
+                    >
+                        {project.name}
+                    </Link>
                 </div>
-            </>
+                <div>
+                    <ul className="text-sm list-disc mx-4">
+                        <li className="marker:text-pink">{project.language}</li>
+                        <li className="marker:text-pink">
+                            Date started: {project.date}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     });
     return <>{projectList}</>;
