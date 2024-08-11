db93d38a64c67d6fc38b781afc269de0ff71b1c9
Natalie Raine <nyatalieeee@proton.me>
2024-08-11 10:49:18 -0500

    i should not have needed to do this smfh

---
+++ src/data/linkData.ts
@@ -0,0 +1,19 @@
+enum LinkNames {
+    DISC = 'Discord',
+    GITGAY = 'Git.gay',
+    GH = 'Github',
+    MASTO = 'Mastodon (tech.lgbt)',
+    TWT = 'Twitter (NOT x)',
+    PRNS = 'Pronouns.page',
+    AMY = '<3',
+}
+
+enum Links {
+    DISC = 'https://discord.com/',
+    GITGAY = 'https://git.gay/nyatalie',
+    GH = 'https://github.com/Nyatalieeee',
+    MASTO = 'https://tech.lgbt/@ideltic',
+    TWT = 'https://x.com/Nyatalieeeee',
+    PRNS = 'https://en.pronouns.page/@Nyatalieeee',
+    AMY = 'https://tech.lgbt/@nightlake',
+}
--- src/data/navbarData.ts
+++
@@ -1,9 +0,0 @@
-enum LinkNames {
-    DISC = 'Discord',
-    GITGAY = 'Git.gay',
-    GH = 'Github',
-    MASTO = 'Mastodon (tech.lgbt)',
-    TWT = 'Twitter (NOT x)',
-    PRNS = 'Pronouns.page',
-    AMY = '<3',
-}
