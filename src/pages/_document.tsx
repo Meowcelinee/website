075453097481fff0d4af3a69a28607a55635bae8
Natalie Raine <nyatalieeee@proton.me>
2024-08-12 16:16:36 -0500

    remove margin (i love tailwind)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Footer from "../components/footer";
-import Header from "../components/header";
+import { Html, Head, Main, NextScript } from 'next/document';
+import Footer from '../components/footer';
+import Header from '../components/header';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body className="bg-mantle text-text m-0">
+            <body className="bg-mantle text-text">
                 <Main />
                 <NextScript />
             </body>
