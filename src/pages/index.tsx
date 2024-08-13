274d91ef69dd41292ea17e04ada42d2eb2565c32
Natalie Raine <nyatalieeee@proton.me>
2024-08-13 15:07:34 -0500

    fix margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
-import Link from 'next/link';
 import Projects from '../components/homepage/projects';
 import Links from '../components/homepage/links';
-import classNames from 'classnames';
 
 export default function Homepage() {
     return (
         <>
             <section>
-                <div className="flex justify-center bg-base px-8">
-                    <div className="flex md:flex-row flex-col gap-12 md:px-8 px-4 md:py-3 py-2 w-full">
-                        <div className="flex flex-col gap-2 md:my-3 my-2 w-11/12 md:w-1/2">
-                            <h1 className="md:text-3xl text-2xl text-pink font-bold mx-1 my-2 md:my-3 md:mb-5">
+                <div className='flex justify-center bg-base px-8'>
+                    <div className='flex md:flex-row flex-col gap-12 md:px-8 px-4 md:py-3 py-2 w-full'>
+                        <div className='flex flex-col gap-2 md:my-3 my-2 mx-auto w-11/12 md:w-1/2'>
+                            <h1 className='md:text-3xl text-2xl text-pink font-bold mx-1 my-2 md:my-3 md:mb-5'>
                                 Links
                             </h1>
                             <Links />
                         </div>
-                        <div className="flex flex-col gap-4 md:my-3 my-2 w-11/12 md:w-1/2">
-                            <h1 className="md:text-3xl text-2xl text-pink font-bold mx-1 my-2 md:my-3">
+                        <div className='flex flex-col gap-4 md:my-3 my-2 mx-auto w-11/12 md:w-1/2'>
+                            <h1 className='md:text-3xl text-2xl text-pink font-bold mx-1 my-2 md:my-3'>
                                 Projects
                             </h1>
                             <Projects />
