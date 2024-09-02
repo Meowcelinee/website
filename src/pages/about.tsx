945eb1e911b0b1958656682aeaef603b2ede99a8
Natalie Raine <nyatalieeee@proton.me>
2024-09-02 15:54:07 -0400

    add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+    return (
+        <>
+            <div className="flex bg-base justify-center align-middle md:p-5 p-4">
+                <div className="bg-gradient-to-br from-base via-mantle to-crust border-red border-2 drop-shadow-md rounded-xl md:p-5 p-4">
+                    <p>idk what to do here tbh</p>
+                </div>
+            </div>
+        </>
+    );
+}
