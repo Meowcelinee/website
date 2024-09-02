2d54adb1bf9b326cc2df28aa2385fbc03b5054e4
Natalie Raine <nyatalieeee@proton.me>
2024-09-02 15:54:02 -0400

    clean up navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,29 +3,27 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <>
-            <div className='flex text-center justify-between mx-auto my-2'>
-                <div className='flex justify-start mx-auto w-auto'></div>
-                <div className='flex gap-7 justify-center mx-5 w-full'>
+            <div className='flex text-center mx-auto my-2'>
+                <div className='flex gap-7 justify-center mx-auto w-full'>
+                    <Link
+                        className='font-normal text-subtext0 text-lg transition duration-300 hover:text-text'
+                        href='/'
+                    >
+                        home
+                    </Link>
+                    <Link
+                        className='font-normal text-subtext0 text-lg transition duration-300 hover:text-text'
+                        href='/about'
+                    >
+                        about me
+                    </Link>
                     <Link
                         className='font-normal text-subtext0 text-lg transition duration-300 hover:text-text'
                         href='/wip'
                     >
                         sample text
                     </Link>
-                    <Link
-                        className='font-normal text-subtext0 text-lg transition duration-300 hover:text-text'
-                        href='/wip'
-                    >
-                        texticles
-                    </Link>
-                    <Link
-                        className='font-normal text-subtext0 text-lg transition duration-300 hover:text-text'
-                        href='/wip'
-                    >
-                        text-text
-                    </Link>
                 </div>
-                <div className='flex justify-end mx-5 w-auto'></div>
             </div>
         </>
     );
