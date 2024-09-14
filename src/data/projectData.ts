e41d9825228adce0a6538b723d64b59a3ea020ef
Natalie Raine <nyatalieeee@proton.me>
2024-09-13 21:02:35 -0400

    simplify & add new project

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,53 +1,45 @@
-enum Names {
-    COLON3 = 'Colon3 Bot',
-    GAEENT = 'Original Gae Enterprises Site',
-    PAGESGAY = 'My Site (pages.gay)',
-    PICREW = 'Picrew Userstyle',
-    THIS = 'This Site',
-}
-
-enum Languages {
-    JS = 'Javascript',
-    CSS = 'CSS',
-    HTMLCSS = 'HTML/CSS',
-    TSX = 'Typescript/TSX',
-}
-
-const projects = [
+const projects: Object[] = [
     {
-        name: Names.COLON3,
+        name: 'Colon3 Bot',
         link: 'https://github.com/Nyatalieeee/colon3-bot',
-        language: Languages.JS,
+        language: 'Javascript',
         date: '06/06/2024',
         id: 1,
     },
     {
-        name: Names.GAEENT,
-        link: 'https://gae-ent.vercel.app/',
-        language: Languages.HTMLCSS,
-        date: '04/17/2024',
+        name: 'My Original Site (pages.gay)',
+        link: 'https://nyatalie.pages.gay',
+        language: 'HTML/CSS',
+        date: '03/13/2024',
         id: 2,
     },
     {
-        name: Names.PAGESGAY,
-        link: 'https://nyatalie.pages.gay',
-        language: Languages.HTMLCSS,
-        date: '03/13/2024',
+        name: 'This Site (i love recursion)',
+        link: '/',
+        language: 'Typescript/TSX',
+        date: '08/04/2024',
         id: 3,
     },
     {
-        name: Names.PICREW,
-        link: 'https://github.com/catppuccin/userstyles/tree/main/styles/picrew',
-        language: Languages.CSS,
-        date: '02/25/2024',
+        name: 'Original Gae Enterprises Site',
+        link: 'https://gae-ent.vercel.app/',
+        language: 'HTML/CSS',
+        date: '04/17/2024',
         id: 4,
     },
     {
-        name: Names.THIS,
-        link: '/',
-        language: Languages.TSX,
-        date: '08/04/2024',
+        name: 'Picrew Userstyle',
+        link: 'https://github.com/catppuccin/userstyles/tree/main/styles/picrew',
+        language: 'CSS',
+        date: '02/25/2024',
         id: 5,
+    },
+    {
+        name: 'NameMC Userstyle',
+        link: 'https://github.com/catppuccin/userstyles/tree/main/styles/namemc',
+        language: 'CSS',
+        date: '08/13/2024',
+        id: 6,
     },
 ];
 
