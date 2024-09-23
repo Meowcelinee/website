1d17891ea5e50b9f90fa94889076c484753034b3
Natalie Raine <nyatalieeee@proton.me>
2024-09-22 21:27:17 -0400

    remove import

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,6 @@
-import { useRouter } from 'next/router';
 import NavbarLink from './navlinks';
 
 export default function Navbar() {
-    const router = useRouter();
     return (
         <div className='flex text-center mx-auto my-2'>
             <div className='flex gap-7 justify-center mx-auto w-full'>
