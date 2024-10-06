78295dec64e82bd5b9514cc076065b3a2e953196
Natalie Raine <nyatalieeee@proton.me>
2024-10-06 00:01:08 -0400

    change divs to buttons

--- src/pages/projects/calculator.tsx
+++ src/pages/projects/calculator.tsx
@@ -13,54 +13,54 @@ export default function Calculator() {
                     </span>
                 </div>
                 <div className='flex flex-wrap bg-mantle justify-between rounded-b-lg p-2 mt-2'>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         7
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         8
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         9
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         X
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         4
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         5
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         6
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         -
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         1
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         2
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         3
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         +
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         0
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         .
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         🠠
-                    </div>
-                    <div className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
+                    </button>
+                    <button className='bg-surface0 text-center rounded-lg px-5 py-3 my-2 mx-1 w-1/5 transition-all duration-150 hover:bg-surface2 active:bg-surface1'>
                         =
-                    </div>
+                    </button>
                 </div>
             </div>
         </div>
