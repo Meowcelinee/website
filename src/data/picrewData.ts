03f788663c0ea93d8c9c7bd7ca198b5b44d6f3f9
Natalie Raine <nyatalieeee@proton.me>
2024-11-24 15:06:21 -0500

    :3

---
+++ src/data/picrewData.ts
@@ -0,0 +1,53 @@
+interface Picrew {
+    author: string;
+    image: string;
+    name: string;
+    link: string;
+}
+
+const picrews: Picrew[] = [
+    {
+        author: '✦絢瀬',
+        image: '/images/picrews/1904634.png',
+        name: 'キラキラ鱈メーカー3',
+        link: 'https://picrew.me/en/image_maker/1904634'
+    },
+    {
+        author: 'anjubatus',
+        image: '/images/picrews/1918788.png',
+        name: 'Heart Hold Character',
+        link: 'https://picrew.me/en/image_maker/1918788/',
+    },
+    {
+        author: 'hmng',
+        image: '/images/picrews/229486.png',
+        name: 'ガオmaker',
+        link: 'https://picrew.me/en/image_maker/229486'
+    },
+    {
+        author: 'reelrollsweat',
+        image: '/images/picrews/2069970.png',
+        name: 'little guy maker',
+        link: 'https://picrew.me/en/image_maker/2069970'
+    },
+    {
+        author: 'Blob123',
+        image: '/images/picrews/2439112.png',
+        name: 'Make a person :)',
+        link: 'https://picrew.me/en/image_maker/2439112'
+    },
+    {
+        author: 'おさむくん',
+        image: '/images/picrews/1787745.png',
+        name: 'おさむメーカー',
+        link: 'https://picrew.me/en/image_maker/1787745'
+    },
+    {
+        author: '✦絢瀬',
+        image: '/images/picrews/2307052.png',
+        name: 'ふわふわ鱈メーカー4',
+        link: 'https://picrew.me/en/image_maker/2307052'
+    }
+];
+
+export default picrews;
