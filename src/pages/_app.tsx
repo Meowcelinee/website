aade75324dec55b45029325d38551ccfb84e3036
Nyatalieeee <nyatalieeee@proton.me>
2025-01-03 21:52:12 -0500

    fix imports + remove wip page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from 'next';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import Footer from '../components/footer';
-import Header from '../components/header/header';
-import '../styles/globals.css';
+import Footer from '@/src/components/footer';
+import Header from '@/src/components/header/header';
+import '@/src/styles/globals.css';
 
 export const metadata: Metadata = {
     title: "natalie's site :3",
     description: 'made with next',
 };
 
-const hfIgnore = ['/404', '/wip'];
-
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
     return (
         <>
-            {hfIgnore.indexOf(router.pathname) > -1 ? (
+            {router.pathname === '/404' ? (
                 <Component {...pageProps} />
             ) : (
                 <>
