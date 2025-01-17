bc3a2f2e632c7bda770947b11cb733ae11a9cd40
Marceline Raine <nyatalieeee@proton.me>
2025-01-17 18:02:37 -0500

    man i hope this works

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,29 @@
-import type { Metadata } from 'next';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Footer from '@/src/components/footer';
 import Header from '@/src/components/header/header';
 import '@/src/styles/globals.css';
-
-export const metadata: Metadata = {
-    title: "natalie's site :3",
-    description: 'made with next',
-};
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
     return (
         <>
+            <Head>
+                <title>my site :3</title>
+                <meta name='description' content='made with next' />
+
+                <meta property='og:title' content="natalie's site" />
+                <meta
+                    property='og:description'
+                    content='shittily made with nextjs'
+                />
+                <meta
+                    property='og:image'
+                    content='/images/picrews/2439112.png'
+                />
+            </Head>
+
             {router.pathname === '/404' ? (
                 <Component {...pageProps} />
             ) : (
