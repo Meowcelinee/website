539163bc6a0af392eaf9a000e4976e7f3ac53728
Marceline Raine <nyatalieeee@proton.me>
2025-01-17 18:02:23 -0500

    getAge

--- src/data/natalie.ts
+++ src/data/natalie.ts
@@ -1,16 +1,13 @@
 type three = 3; // this is totally necessary
 
-interface Orientation {
-    romantic: string;
-    sexual: string;
-}
+type OrientationType = { romantic: string; sexuality: string };
 
 interface Nerd {
     firstName: string;
     middleName: string;
     lastName: string;
     birthday: string;
-    orientation: Orientation;
+    orientation: OrientationType;
     pronouns: string[];
     interests?: string[];
     colon?: three;
@@ -23,7 +20,7 @@ export const natalie: Nerd = {
     birthday: '2006-01-13',
     orientation: {
         romantic: 'Lesbian',
-        sexual: 'Demisexual',
+        sexuality: 'Demisexual',
     },
     pronouns: ['she/her'],
     interests: ['Music', 'Programming', 'Destiny 2', 'Minecraft', 'Women'],
@@ -31,6 +28,29 @@ export const natalie: Nerd = {
 };
 
 const date = new Date();
+
+export function getAge() {
+    const birthday = [1, 17, 2006];
+    const currentDate = new Date().toLocaleDateString().split('/');
+    const today = [
+        Number(currentDate[0]),
+        Number(currentDate[1]),
+        Number(currentDate[2]),
+    ];
+
+    /*
+     *  IF this month's number is GREATER THAN the birthday month's number,
+     *  AND today's number (of the month) is GREATER THAN the birthday's number,
+     *  this means the current date is *after* the birthday date,
+     *  and the larger age should be returned.
+     */
+    if (today[0] >= birthday[0]) {
+        if (today[1] >= birthday[1]) {
+            return today[2] - birthday[2];
+        }
+    }
+    return today[2] - birthday[2] - 1;
+}
 
 export function isBirthday(): boolean {
     const birthday = '1/13';
