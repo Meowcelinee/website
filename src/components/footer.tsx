8fbaa8aae8709036d303932c7d23f5147ae0e9ff
Marceline Raine <nyatalieeee@proton.me>
2025-01-20 13:47:43 -0500

    improve footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,15 +4,15 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className='bg-mantle mt-3 mx-5 md:py-3 md:px-2 py-3 px-2'>
-            <p className='text-subtext1 mx-1'>
+        <div className='bg-mantle mt-1 mx-2 md:py-3 md:px-3 py-2 px-2'>
+            <p className='text-subtext1'>
                 &copy; {currentYear} marceline raine :3{' '}
                 <span className='text-xs text-overlay2 opacity-60'>
                     (not actually but idc)
                 </span>
             </p>
             <hr className='my-2 border-overlay0' />
-            <p className='text-subtext0 text-sm mx-1 my-1'>
+            <p className='text-subtext0 text-sm my-1'>
                 hosted by{' '}
                 <Link
                     className='text-peach underline transition-all duration-300 hover:text-text'
@@ -22,7 +22,7 @@ export default function Footer() {
                     cloudflare pages
                 </Link>
             </p>
-            <p className='text-subtext0 text-sm mx-1 my-1'>
+            <p className='text-subtext0 text-sm my-1'>
                 made with <span className='text-pink'>{`<`}3</span>
             </p>
         </div>
