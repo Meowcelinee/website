293f884133d4928d4b0dfe9003db31cf07c2529e
Marceline Raine <nyatalieeee@proton.me>
2025-01-24 10:45:41 -0500

    new picrew

--- src/data/picrewData.ts
+++ src/data/picrewData.ts
@@ -10,7 +10,7 @@ const picrews: Picrew[] = [
         author: '✦絢瀬',
         image: '/images/picrews/1904634.png',
         name: 'キラキラ鱈メーカー3',
-        link: 'https://picrew.me/en/image_maker/1904634'
+        link: 'https://picrew.me/en/image_maker/1904634',
     },
     {
         author: 'anjubatus',
@@ -22,32 +22,38 @@ const picrews: Picrew[] = [
         author: 'hmng',
         image: '/images/picrews/229486.png',
         name: 'ガオmaker',
-        link: 'https://picrew.me/en/image_maker/229486'
+        link: 'https://picrew.me/en/image_maker/229486',
     },
     {
         author: 'reelrollsweat',
         image: '/images/picrews/2069970.png',
         name: 'little guy maker',
-        link: 'https://picrew.me/en/image_maker/2069970'
+        link: 'https://picrew.me/en/image_maker/2069970',
     },
     {
         author: 'Blob123',
         image: '/images/picrews/2439112.png',
         name: 'Make a person :)',
-        link: 'https://picrew.me/en/image_maker/2439112'
+        link: 'https://picrew.me/en/image_maker/2439112',
     },
     {
         author: 'おさむくん',
         image: '/images/picrews/1787745.png',
         name: 'おさむメーカー',
-        link: 'https://picrew.me/en/image_maker/1787745'
+        link: 'https://picrew.me/en/image_maker/1787745',
     },
     {
         author: '✦絢瀬',
         image: '/images/picrews/2307052.png',
         name: 'ふわふわ鱈メーカー4',
-        link: 'https://picrew.me/en/image_maker/2307052'
-    }
+        link: 'https://picrew.me/en/image_maker/2307052',
+    },
+    {
+        author: 'オム卵 ',
+        image: '/images/picrews/2410587.png',
+        name: '*StarGazerスターゲイザー*',
+        link: 'https://picrew.me/en/image_maker/2410587',
+    },
 ];
 
 export default picrews;
