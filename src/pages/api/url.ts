b6d611056160a8967aba431c09c3c2bf007bae73
Meowcelinee <meowcelinee@gmail.com>
2025-08-12 04:09:44 -0400

    pls

--- src/pages/api/url.ts
+++ src/pages/api/url.ts
@@ -1,7 +1,13 @@
+// i genuinely hate this with all my heart. fuck webdev
+
 const URL = process.env.REQUEST_URL as string;
 
 export const runtime = 'experimental-edge';
 
 export default async function handler() {
-    return new Response(JSON.stringify({ URL: URL }));
+    const headers = new Headers({ 'Access-Control-Allow-Origin': '*' });
+
+    return new Response(JSON.stringify({ URL: URL }), {
+        headers: headers,
+    });
 }
